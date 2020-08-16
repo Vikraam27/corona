8f59af1764659180ff6c7e00a971b9fd27789160
Vikraam27 <49831979+Vikraam27@users.noreply.github.com>
2020-08-16 13:43:17 +0700

    Add files via upload

---
+++ precache-manifest.4efe5d57f8055cdda9101aee6798b5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d908a3c1de23d48073ec5924c9ac284",
+    "url": "/corona.github.io/index.html"
+  },
+  {
+    "revision": "21a0881a504286377b13",
+    "url": "/corona.github.io/static/css/main.82820df7.chunk.css"
+  },
+  {
+    "revision": "2d171325554e9383a2c0",
+    "url": "/corona.github.io/static/js/2.70ec4be4.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/corona.github.io/static/js/2.70ec4be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a0881a504286377b13",
+    "url": "/corona.github.io/static/js/main.ad7936b2.chunk.js"
+  },
+  {
+    "revision": "847efe593694cc9d8a08",
+    "url": "/corona.github.io/static/js/runtime-main.870a1fa2.js"
+  }
+]);
